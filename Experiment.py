c7b7b8588816c8e31f6b62c9a792ed0c35b42c26
subhashjaini <subhashkjaini@gmail.com>
2016-04-02 10:45:33 -0400

    Update Experiment.py

--- Experiment.py
+++ Experiment.py
@@ -10,3 +10,9 @@ d = {
 
 }
 
+AA = pd.DataFrame(d)
+ColumnNames = list(AA.columns)
+print(ColumnNames)
+for Column in range(0,len(ColumnNames)):
+	for ValueInColumn in AA.ix[:,Column].values:
+		print(ValueInColumn)
