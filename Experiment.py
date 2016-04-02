195f7bb9d4316f98387e9a71a4e54efca4e0d0d5
subhashjaini <subhashkjaini@gmail.com>
2016-04-02 10:40:10 -0400

    Create Experiment.py

---
+++ Experiment.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import random
+d = {
+'First' : random.sample(range(1, 10000000), 8),'Second' : random.sample(range(-10000000, -1), 8),"Text" :
+[
+'The dfgAlteryx','Analytics',
+'Achieving your financial goals is no easy task. It is complicated and takes time. How successful you are will depend on the choices you make, today and If prompted for the company name, enter  to register. Cont or  with questions Lastttttttttttt','Awards recognize','the','outstanding','achievements','of'
+]
+
+
+}
+
